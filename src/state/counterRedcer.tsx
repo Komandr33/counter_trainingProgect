014e2fef12f41fec030e626f551b13054856f0b0
Komandr33 <and.kom2017@gmail.com>
2024-07-13 01:32:03 +0300

    update counterRedcer.tsx

--- src/state/counterRedcer.tsx
+++ src/state/counterRedcer.tsx
@@ -1,13 +1,18 @@
 const initialState = {
   extensionValue: {start: 0, max: 0},
   value: 0,
-  error: false
+  disInc: true,
+  disReset: true,
+  error: true
 }
+
 export type extensionValueType = { start: number, max: number }
 type counterType = {
   extensionValue: extensionValueType
   value: number
   error: boolean
+  disInc: boolean
+  disReset: boolean
 }
 
 export const counterReducer = (state: counterType = initialState, action: ActionsType): counterType => {
@@ -20,6 +25,10 @@ export const counterReducer = (state: counterType = initialState, action: Action
       return {...state, value: state.value + 1}
     case 'RESET-COUNTER':
       return {...state, value: state.extensionValue.start}
+    case 'DISABLED-INC':
+      return {...state, disInc: action.val}
+    case 'DISABLED-RESET':
+      return {...state, disReset: action.val}
     case 'ERROR':
       return {...state, error: action.val}
     default:
@@ -31,15 +40,17 @@ export const updateSettings = (val: extensionValueType) => ({type: 'UPDATE-SETTI
 export const setSettings = (val: number) => ({type: 'SET-SETTINGS', val} as const)
 export const incCounter = () => ({type: 'INC-COUNTER'} as const)
 export const resetCounter = () => ({type: 'RESET-COUNTER'} as const)
-export const disabled = (item: string) => ({type: 'DISABLED', item} as const)
+export const disabledReset = (val: boolean) => ({type: 'DISABLED-RESET', val} as const)
 export const setError = (val: boolean) => ({type: 'ERROR', val} as const)
+export const disabledInc = (val: boolean) => ({type: 'DISABLED-INC', val} as const)
 
 type ActionsType =
   | updateValueType
   | ReturnType<typeof incCounter>
   | ReturnType<typeof resetCounter>
-  | ReturnType<typeof disabled>
+  | ReturnType<typeof disabledReset>
   | ReturnType<typeof setSettings>
   | ReturnType<typeof setError>
+  | ReturnType<typeof disabledInc>
 
 export type updateValueType = ReturnType<typeof updateSettings>
