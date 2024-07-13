006ac0ca17af67ef48c3437c4f3454e6b07587fc
Komandr33 <and.kom2017@gmail.com>
2024-07-14 02:05:37 +0300

    added LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.module.css';
 import {SettingsCounter} from './components/extensionCounter/SettingsCounter';
 import {Counter} from './components/counter/Counter';
 import S from './App.module.css'
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {AppStateType} from './state/store';
+import {setSettings, updateSettings} from './state/counterReducer';
 
 export const App = () => {
 
   const count = useSelector((state: AppStateType) => state.counter)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const counterSettings = localStorage.getItem('Counter settings')
+    if (counterSettings) {
+      dispatch(updateSettings(JSON.parse(counterSettings)))
+      dispatch(setSettings())
+    }
+  }, [dispatch])
 
   return (
     <div className={S.appWrapper}>
